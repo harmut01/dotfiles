a3afe2a0c57f51474d98024578344a8498d80bec
Harrison Mutai <harrison.mutai@arm.com>
2024-01-17 14:12:17 +0000

    journal: migrate jr to macos

--- .config/fish/functions/jr.fish
+++ .config/fish/functions/jr.fish
@@ -1,11 +1,26 @@
-function jr
-    if ! set -q notes_path
-        set notes_path ~/projects/notes
-    end
+function jr --wraps=vi\ -Os\ journal/\(date\ --date=\'-1\ day\'\ +\%F\).md\ journal/\(date\ +\%F\).md\ ops-work.md --description 'Open my journal in split mode'
+    set journal_path ~/projects/notes/journal
 
-    if test -f ~/projects/notes/(date +%F).md
+    find $journal_path -type f \( ! -iname template \) -size -200c -delete
+
+    set day_note $journal_path/(date +%F).md
+    set look_back 1
+    set prev_note $journal_path/(date -v "-"$look_back"d" +%F).md
+
+    if not test -f $day_note
         note
     end
 
-    vi -O $notes_path/ops-personal.md $notes_path/journal/(date +%F).md $argv
+    while not test -f $prev_note
+        set look_back (math $look_back+1)
+        set prev_note $journal_path/(date -v "-"$look_back"d" +%F).md
+
+        if test $look_back -gt 30
+            vi -O ~/projects/notes/ops-work.md $day_note
+            return
+        end
+    end
+
+    echo vi ~/projects/notes/ops-work.md -c vs\ $prev_note\ \|\ sp\ $day_note $argv
+    vi ~/projects/notes/ops-work.md -c vs\ $prev_note\ \|\ sp\ $day_note $argv
 end
