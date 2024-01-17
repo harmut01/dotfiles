c8daae22ac5172e27e546f6ab1473eb57ef52dd0
Harrison Mutai <harrison.mutai@arm.com>
2024-01-17 12:31:49 +0000

    automate installation of command line text utilities

--- bootstrap.fish
+++ bootstrap.fish
@@ -1,5 +1,11 @@
 #!/usr/bin/env fish
 git pull origin main
+if type -q brew
+    brew update
+else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew install tmux
+end
 
 rsync --exclude ".git/" \
 	--exclude ".DS_Store" \
@@ -10,10 +16,18 @@ rsync --exclude ".git/" \
 	-avh --no-perms . ~;
 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install;
-fish install --noninteractive
-rm -f install
-
-brew update
+fish install --noninteractive && rm -f install
 
 omf install bobthefish fzf 
 omf install https://github.com/jhillyerd/plugin-git
+
+# Pre-installed macOS system Vim does not support Python 3.
+brew install vim
+
+# Install Vim plugins
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+brew install cmake python go nodejs
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all
