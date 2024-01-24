25daf2d7459036b7180301d07b8118a30f27af72
Harrison Mutai <harrison.mutai@arm.com>
2024-01-24 10:52:38 +0000

    fish: add generic TF_WORKSPACE env variable

--- .config/fish/functions/ci.fish
+++ .config/fish/functions/ci.fish
@@ -1,4 +1,3 @@
 function ci --wraps='cd ~/projects/tf-a-projects/platform-ci' --description 'alias ci=cd ~/projects/tf-a-projects/platform-ci'
-  cd ~/projects/tf-a-projects/platform-ci $argv
-        
+  cd $TF_WORKSPACE/platform-ci $argv
 end
--- .config/fish/functions/tft.fish
+++ .config/fish/functions/tft.fish
@@ -1,4 +1,3 @@
 function tft --wraps='cd ~/projects/tf-a-projects/tf-a-tests/' --description 'alias tft=cd ~/projects/tf-a-projects/tf-a-tests/'
-  cd ~/projects/tf-a-projects/tf-a-tests/ $argv
-        
+  cd $TF_WORKSPACE/tf-a-tests/ $argv
 end
