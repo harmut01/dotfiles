83bfdd9c25ae78fddac9088489c2f3f7680dbf7f
Harrison Mutai <harrison.mutai@arm.com>
2024-01-24 17:28:07 +0000

    fish: fix handling of PROJECT env var

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    if test -d ~/projects
+        set -U PROJECTS ~/projects
+    else
+        set -U PROJECTS /workspaces
+    end
 end
