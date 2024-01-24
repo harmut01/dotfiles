78ad080a80888519cbde4673862c8d5670b6167f
Harrison Mutai <harrison.mutai@arm.com>
2024-01-24 20:40:58 +0000

    gwip.fish: remove wip alias

--- .config/fish/functions/gwip.fish
+++
@@ -1,5 +0,0 @@
-function gwip --description 'git commit a work-in-progress branch'
-    git add -u
-    git rm (git ls-files --deleted) 2>/dev/null
-    git commit -m "fixup chore(no-merge): work in progress" --no-verify
-end
