6bccb9554fad76168b7b2e634278fcc0546eb439
Harrison Mutai <harrison.mutai@arm.com>
2024-11-01 09:25:22 +0000

    bootstrap.fish: add script to bootstrap using fish

---
+++ bootstrap.fish
@@ -0,0 +1,6 @@
+curl -L https://get.oh-my.fish | fish
+omf install bobthefish # Theme
+omf install bass      # Allows running heavily Bash-dependant utilities, like nvm
+omf install colored-man-pages
+omf install https://github.com/jhillyerd/plugin-git
+omf install z         # Autojump implementation
