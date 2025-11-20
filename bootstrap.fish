fa993ceee0c3b16070ec2d500298bf828d15433b
Harrison Mutai <harrison.mutai@arm.com>
2025-11-20 11:34:43 +0000

    chore: add lazyvim config and brew update

--- bootstrap.fish
+++ bootstrap.fish
@@ -24,13 +24,34 @@ fish install --noninteractive && rm -f install
 omf install bobthefish fzf
 omf install https://github.com/jhillyerd/plugin-git
 
+# Save Homebrew’s installed location.
+brew upgrade
+set -u BREW_PREFIX (brew --prefix)
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+ln -s "$BREW_PREFIX/bin/gsha256sum" "$BREW_PREFIX/bin/sha256sum"
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed --with-default-names
+# # Install a modern version of Bash.
+brew install bash
+brew install bash-completion2
+
 # Pre-installed macOS system Vim does not support Python 3.
 brew install vim
+
+brew install cmake python go nodejs
 
 # Install Vim plugins
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-brew install cmake python go nodejs
+
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --all
